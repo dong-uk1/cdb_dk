dfafad9fca51a05175371b5e892ab01b1f4b6d72
kyeongseo <keongseo1234@gmail.com>
2023-01-02 17:11:06 +0900

    map select list

--- src/main/java/com/multi/cdb/BorrowDAO.java
+++ src/main/java/com/multi/cdb/BorrowDAO.java
@@ -1,5 +1,30 @@
 package com.multi.cdb;
 
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Component
 public class BorrowDAO {
+	
+	@Autowired
+	SqlSessionTemplate my;
+	
+	// GetData 자바 파일에서 요청함.
+	// 공공 api에서 불러와 db에 레코드로 저장하는 메서드
+	public void insert(BorrowVO vo) {
+		my.insert("borrow.insert", vo);
+	}
+	
+	// 검색결과에 맞는 값을 반환해줄 메서드
+	// 요청 변수로 광역시 변수와 시군구 변수로 요청
+	public List<BorrowVO> sel_list(BorrowVO vo) {
+		return my.selectList("borrow.sel_list", vo);
+	}
 }
--- src/main/java/com/multi/cdb/BuyDAO.java
+++ src/main/java/com/multi/cdb/BuyDAO.java
@@ -1,5 +1,7 @@
 package com.multi.cdb;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,9 @@ public class BuyDAO {
 	
 	public void insert(BuyVO vo) {
 		my.insert("buy.insert", vo);
+	}
+	
+	public List<BuyVO> sel_list(BuyVO vo) {
+		return my.selectList("buy.sel_list");
 	}
 }
