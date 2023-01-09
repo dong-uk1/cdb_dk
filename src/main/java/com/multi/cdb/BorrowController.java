ccbb0e0e59d6c273a62528e6185804063615bfe4
kyeongseo <keongseo1234@gmail.com>
2023-01-09 13:21:16 +0900

    [#7] modify controller and uri name

--- src/main/java/com/multi/cdb/BorrowController.java
+++ src/main/java/com/multi/cdb/BorrowController.java
@@ -19,33 +19,34 @@ public class BorrowController {
 	@Autowired
 	BorrowDAO dao;
 	
-	// 요청 파라미터로 광역시와 시군구 파라미터로 요청
-	@RequestMapping(value = "/h_rent", method = { RequestMethod.GET })
+	// 쿼리 파라미터를 광역시로 요청함.
+	@RequestMapping(value = "/rent_city", method = { RequestMethod.GET })
 	@ResponseBody
 	public Object sel_list(@RequestParam("br_brtc") String br_brtc) {
-		List<BorrowVO> list = dao.sel_list(br_brtc);
+		List<BorrowVO> list = dao.city(br_brtc);
 		return list;
 	}
 
-	// 세부 게시물 조
-	@RequestMapping(value = "/rent_list_id", method= { RequestMethod.GET })
+	// 쿼리 파라미터를 공고아이디와 광역시로 요청함.
+	// 다중 파라미터라서 Map(HashMap)으로 받음. 
+	@RequestMapping(value = "/rent_view", method= { RequestMethod.GET })
 	public Object sel_list2(@RequestParam("br_pbid") String br_pbid, @RequestParam("br_brtc") String br_brtc, Model model) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("br_pbid", br_pbid);
 		map.put("br_brtc", br_brtc);
-		List<BorrowVO> list = dao.list_id(map);
+		List<BorrowVO> list = dao.detail(map);
+		String name = dao.show_pbname(br_pbid);
 		model.addAttribute("list", list);
+		model.addAttribute("name", name);
 		return list;
 	}
 		
-	// 전체 데이터 조회
+	// 전국 전체 데이터 조회
 	@RequestMapping(value = "/rent_all", method = { RequestMethod.GET })
 	@ResponseBody
 	public Object all() {
 		List<BorrowVO> list = dao.all();
 		return list;
 	}
-	
-	
 	
 }
--- src/main/java/com/multi/cdb/BuyController.java
+++ src/main/java/com/multi/cdb/BuyController.java
@@ -15,36 +15,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 //@RequestMapping("/sale")
 public class BuyController {
-	
+
 	@Autowired
 	BuyDAO dao;
-	
-	// 광역시도 코드로 파라미터 요청 후 검색
-	@RequestMapping(value = "/h_sale", method= { RequestMethod.GET })
+
+	// 쿼리 파라미터를 광역시로 요청함.
+	@RequestMapping(value = "/sale_city", method = { RequestMethod.GET })
 	@ResponseBody
 	public Object sel_list(@RequestParam("by_brtc") String by_brtc) {
-		List<BuyVO> list = dao.sel_list(by_brtc);
+		List<BuyVO> list = dao.city(by_brtc);
 		return list;
 	}
 
-	// 세부 게시물 조회
-	@RequestMapping(value = "/list_id", method = { RequestMethod.GET })
-	public Object sel_list2(@RequestParam("by_pbid") String by_pbid, @RequestParam("by_brtc") String by_brtc, Model model) {
+	// 쿼리 파라미터를 공고아이디와 광역시로 요청함.
+	// 다중 파라미터라서 Map(HashMap)으로 받음.
+	@RequestMapping(value = "/sale_view", method = { RequestMethod.GET })
+	public Object sel_list2(@RequestParam("by_pbid") String by_pbid, @RequestParam("by_brtc") String by_brtc,
+			Model model) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("by_pbid", by_pbid);
 		map.put("by_brtc", by_brtc);
-		List<BuyVO> list = dao.list_id(map);
+		List<BuyVO> list = dao.detail(map);
+		String name = dao.show_pbname(by_pbid);
 		model.addAttribute("list", list);
+		model.addAttribute("name", name);
 		return list;
 	}
-	
-	// 전국 검색시 조회
-	@RequestMapping(value = "/all", method = { RequestMethod.GET })
+
+	// 전국 전체 데이터 조회
+	@RequestMapping(value = "/sale_all", method = { RequestMethod.GET })
 	@ResponseBody
 	public Object all() {
 		List<BuyVO> list = dao.all();
 		return list;
 	}
-	
-	
+
 }
