351b14efe4a9e13976516456899301aa4e2ac9d4
kyeongseo <keongseo1234@gmail.com>
2023-01-09 13:27:27 +0900

    modify method name

--- src/main/java/com/multi/cdb/BorrowDAO.java
+++ src/main/java/com/multi/cdb/BorrowDAO.java
@@ -1,38 +1,44 @@
 package com.multi.cdb;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Component
 public class BorrowDAO {
-	
+
 	@Autowired
 	SqlSessionTemplate my;
-	
-	// GetData 자바 파일에서 요청함.
-	// 공공 api에서 불러와 db에 레코드로 저장하는 메서드
+
+	// 자바 클래스파일 GetData에서 요청함.
+	// 공공 api에서 데이터를 불러와 db에 삽입
 	public void insert(BorrowVO vo) {
 		my.insert("borrow.insert", vo);
 	}
-	
-	// 검색결과에 맞는 값을 반환해줄 메서드
-	// 요청 변수로 광역시 변수로 요청
-	public List<BorrowVO> sel_list(String br_brtc) {
-		return my.selectList("borrow.sel_list", br_brtc);
-	}
-	
-	public List<BorrowVO> list_id(String br_pbid) {
-		return my.selectList("borrow.list_id", br_pbid);
-	}
-	
+
+	// 전체 레코드 조회
 	public List<BorrowVO> all() {
 		return my.selectList("borrow.all");
+	}
+
+	// 파라미터는 광역시
+	// 광역시로 레코드 조회
+	public List<BorrowVO> city(String br_brtc) {
+		return my.selectList("borrow.city", br_brtc);
+	}
+
+	// 파라미터는 공고아이디와 광역시
+	// 다중 파라미터로 인해 해쉬맵으로 받음.
+	// 공고아이디와 광역시로 레코드 조회
+	public List<BorrowVO> detail(Map<String, Object> map) {
+		return my.selectList("borrow.detail", map);
+	}
+
+	// 중복된 공고명 하나만 조회
+	public String show_pbname(String br_pbid) {
+		return my.selectOne("borrow.show_pbname", br_pbid);
 	}
 }
--- src/main/java/com/multi/cdb/BuyDAO.java
+++ src/main/java/com/multi/cdb/BuyDAO.java
@@ -1,6 +1,7 @@
 package com.multi.cdb;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,23 +9,36 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BuyDAO {
-	
+
 	@Autowired
 	SqlSessionTemplate my;
-	
+
+	// 자바 클래스파일 GetData에서 요청함.
+	// 공공 api에서 데이터를 불러와 db에 삽입
 	public void insert(BuyVO vo) {
 		my.insert("buy.insert", vo);
 	}
-	
-	public List<BuyVO> sel_list(String by_brtc) {
-		return my.selectList("buy.sel_list", by_brtc);
-	}
-	
+
+	// 전체 레코드 조회
 	public List<BuyVO> all() {
 		return my.selectList("buy.all");
 	}
-	
-	public List<BuyVO> list_id(String by_pbid){
-		return my.selectList("buy.list_id", by_pbid);
+
+	// 파라미터는 광역시
+	// 광역시로 레코드 조회
+	public List<BuyVO> city(String by_brtc) {
+		return my.selectList("buy.city", by_brtc);
+	}
+
+	// 파라미터는 공고아이디와 광역시
+	// 다중 파라미터로 인해 해쉬맵으로 받음.
+	// 공고아이디와 광역시로 레코드 조회
+	public List<BuyVO> detail(Map<String, Object> map) {
+		return my.selectList("buy.detail", map);
+	}
+
+	// 중복된 공고명 하나만 조회
+	public String show_pbname(String by_pbid) {
+		return my.selectOne("buy.show_pbname", by_pbid);
 	}
 }
