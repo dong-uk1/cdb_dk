1a4851803aefaa32a0b70d8e8dc4635c749d957b
kyeongseo <keongseo1234@gmail.com>
2023-01-10 13:30:25 +0900

    [#17] add service class

--- src/main/java/com/multi/cdb/BorrowService.java
+++ src/main/java/com/multi/cdb/BorrowService.java
@@ -1,5 +1,32 @@
 package com.multi.cdb;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BorrowService {
+
+	
+	@Autowired
+	private BorrowDAO dao;
+
+	public void insert(BorrowVO vo) {
+		dao.insert(vo);
+	}
+	
+	public List<BorrowVO> all(){
+		return dao.all();
+	}
+	
+	public List<BorrowVO> city(String br_brtc) {
+		return dao.city(br_brtc);
+	}
+	
+	public List<BorrowVO> detail(Map<String, Object> map) {
+		return dao.detail(map);
+	}
 
 }
--- src/main/java/com/multi/cdb/BuyService.java
+++ src/main/java/com/multi/cdb/BuyService.java
@@ -1,5 +1,31 @@
 package com.multi.cdb;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BuyService {
+	
+	@Autowired
+	private BuyDAO dao;
+	
+	public void insert(BuyVO vo) {
+		dao.insert(vo);
+	}
+	
+	public List<BuyVO> all(){
+		return dao.all();
+	}
+	
+	public List<BuyVO> city(String by_brtc) {
+		return dao.city(by_brtc);
+	}
+	
+	public List<BuyVO> detail(Map<String, Object> map) {
+		return dao.detail(map);
+	}
 
 }
